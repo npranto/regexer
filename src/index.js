5781a4dde72e79ed1d9da59197e65adf37dfc49d
Focus <Focus@Nazmuzs-MacBook-Air.local>
2018-05-22 18:48:47 -0400

    adds isEmail function that leverages regex to check if an argument is an email or not

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const isEmail = (email) => {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email.toLowerCase());
+};
+
+module.exports = {
+	isEmail: isEmail
+};
