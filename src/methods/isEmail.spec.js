a71f57bb4f861ba8fc550c5dbdef6923bbe0a52b
Focus <Focus@Nazmuzs-MacBook-Air.local>
2018-05-24 13:33:10 -0400

    adds eslint for linting and fixes layout according to airbnb liniting standards

--- src/methods/isEmail.spec.js
+++ src/methods/isEmail.spec.js
@@ -4,16 +4,16 @@ import isEmail from './isEmail';
 describe('Regexer', () => {
   describe('isEmail()', () => {
     it('should return true when passed \'npranto@gmail.com\'', () => {
-			assert.equal(isEmail('npranto@gmail.com'), true);
-		});
-		it('should return false when passed \'npranto@gmail\'', () => {
-			assert.equal(isEmail('npranto@gmail'), false);
-		});
-		it('should return false when passed \'nprantogmail.com\'', () => {
-			assert.equal(isEmail('nprantogmail.com'), false);
-		});
-		it('should return true when passed \'npranto@123gmail.co\'', () => {
-			assert.equal(isEmail('npranto@123gmail.co'), true);
-		});
+      assert.equal(isEmail('npranto@gmail.com'), true);
+    });
+    it('should return false when passed \'npranto@gmail\'', () => {
+      assert.equal(isEmail('npranto@gmail'), false);
+    });
+    it('should return false when passed \'nprantogmail.com\'', () => {
+      assert.equal(isEmail('nprantogmail.com'), false);
+    });
+    it('should return true when passed \'npranto@123gmail.co\'', () => {
+      assert.equal(isEmail('npranto@123gmail.co'), true);
+    });
   });
 });
