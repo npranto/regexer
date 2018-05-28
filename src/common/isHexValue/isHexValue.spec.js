8ec5a41b6358e20fd17fdfca5176f3b68299b8c3
Focus <Focus@nat-wireless-guest-reg-153-50.bu.edu>
2018-05-28 13:45:45 -0400

    Add isHexValue()

---
+++ src/common/isHexValue/isHexValue.spec.js
@@ -0,0 +1,31 @@
+import { assert } from 'chai';
+import isHexValue from './isHexValue';
+
+describe('Regexer', () => {
+  describe('isHexValue()', () => {
+    it('should return true when passed \'#FBB\'', () => {
+      assert.equal(isHexValue('#FBB'), true);
+    });
+    it('should return false when passed \'#Fka7-t\'', () => {
+      assert.equal(isHexValue('sbfj&&#'), false);
+    });
+    it('should return false when passed \'\'', () => {
+      assert.equal(isHexValue(''), false);
+    });
+    it('should return true when passed \'#SHFF56\'', () => {
+      assert.equal(isHexValue('#SHFF56'), true);
+		});
+		it('should return true when passed \'#FF56\'', () => {
+      assert.equal(isHexValue('#FF56'), true);
+		});
+		it('should return true when passed \'#FF563\'', () => {
+      assert.equal(isHexValue('#FF563'), false);
+		});
+		it('should return true when passed \'#FF563dsff\'', () => {
+      assert.equal(isHexValue('#FF563dsff'), false);
+		});
+		it('should return false when passed null', () => {
+      assert.equal(isHexValue(null), false);
+    });
+  });
+});
