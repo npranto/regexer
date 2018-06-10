0faa7430f9631de4e1488b6cef5a46c16961fc29
Nazmuz Shakib Pranto <npranto@gmail.com>
2018-06-10 09:27:23 -0400

    adds flow for static type checking before build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+// @flow
+
 import _isEmail from './common/isEmail/isEmail';
 import _isUsername from './common/isUsername/isUsername';
 
 export const isEmail = _isEmail;
 export const isUsername = _isUsername;
 
+
+let name:string = 'shakib';
+name = 50;
